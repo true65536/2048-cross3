9d4a8217b91461041c383b53e3c8298384aaacb4
qiyuuu <qiyuuu@gmail.com>
2014-03-20 00:31:08 +0800

    change ga event

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -42,7 +42,7 @@ HTMLActuator.prototype.actuate = function(grids, metadata) {
 // Continues the game (both restart and keep playing)
 HTMLActuator.prototype.continue = function(restart) {
   if (typeof ga !== "undefined") {
-    ga("send", "event", "game", restart ? "restart" : "keep playing");
+    ga("send", "event", window.gameName || "game", restart ? "restart" : "keep playing");
   }
   this.clearMessage();
 };
@@ -143,7 +143,7 @@ HTMLActuator.prototype.message = function(won) {
   var type = won ? "game-won" : "game-over";
   var message = won ? "You win!" : "Game over!";
   if (typeof ga !== "undefined") {
-    ga("send", "event", "game", "end", type, this.score);
+    ga("send", "event", window.gameName || "game", "end", type, this.score);
   }
 
   this.messageContainer.classList.add(type);
